1e47dcf39f235157a326285f7fb05c1d829a28e8
mfgage <105758332+stolendan@users.noreply.github.com>
2024-05-07 14:55:11 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,29 @@
 // Scripts
 // 
 
+/*
+window.addEventListener('resize', resize, false);
+
+video.height = 80; // to get an initial width to work with
+resize();
+
+function resize() {
+videoRatio = video.height / video.width;
+windowRatio = window.innerHeight / window.innerWidth; // browser size 
+
+    if (windowRatio < videoRatio) {
+        if (window.innerHeight > 50) { // smallest video height 
+                video.height = window.innerHeight;
+        } else {
+            video.height = 50;
+    }
+    } else {
+        video.width = window.innerWidth;
+    }
+
+};
+*/
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
